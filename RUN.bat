3c5ff896a9155acf8c62ab1862f6bbdae6d201ef
ToneGabriel <81569579+ToneGabriel@users.noreply.github.com>
2025-04-03 21:31:52 +0300

    Update

--- RUN.bat
+++ RUN.bat
@@ -8,15 +8,22 @@
 :: No need to install venv (is default)
 py -m pip install --upgrade pip
 
-:: Setup VENV ================================================
-:: Set the virtual environment directory name
 set VENV_DIR=.venv
 
-:: Create a new virtual environment and upgrade pip
+:run
+
 if exist %VENV_DIR% (
     echo Virtual environment present.
+
+    call %VENV_DIR%\Scripts\activate
+
+    py ./buildpy --project ./project --json ./build.json
+
+    call %VENV_DIR%\Scripts\deactivate
+
 ) else (
     echo Creating new virtual environment...
+
     py -m venv %VENV_DIR%
 
     call %VENV_DIR%\Scripts\activate
@@ -28,54 +35,8 @@ if exist %VENV_DIR% (
     py -m pip install -r requirements.txt
 
     call %VENV_DIR%\Scripts\deactivate
+
+    goto run
 )
-
-:: Work while venv is active =================================
-call %VENV_DIR%\Scripts\activate
-
-:menu
-
-echo ==========================
-echo        PyForge Menu       
-echo ==========================
-echo 1. Generate CMakeLists
-echo 2. Build Project
-echo 3. Clear Setup and Build
-echo 4. Exit
-echo ==========================
-set /p choice=Choose an option (1-4): 
-
-
-if "%choice%"=="1" (
-    echo Running CMakeLists Generation...
-    call :run_python g
-    pause
-    goto menu
-) else if "%choice%"=="2" (
-    echo Running CMake Build...
-    call :run_python b
-    pause
-    goto menu
-) else if "%choice%"=="3" (
-    echo Running Setup Clear...
-    call :run_python c
-    pause
-    goto menu
-) else if "%choice%"=="4" (
-    echo Exiting...
-    goto end
-) else (
-    echo Invalid choice, please choose between 1-4.
-    pause
-    goto menu
-)
-
-:: Function to call Python script
-:run_python
-py ./buildpy --action %1 --project ./project --json ./build.json
-exit /b
-
-:end
-call %VENV_DIR%\Scripts\deactivate
 
 cmd /k
--- buildpy/__main__.py
+++ buildpy/__main__.py
@@ -1,18 +1,27 @@
 from argparse import ArgumentParser as ap
+import textwrap
 import localimpl
+
+
+def _print_menu() -> None:
+    print(textwrap.dedent(
+    '''
+    ===============================================
+                        PyForge
+    ===============================================
+    Select an option:
+    1. Generate CMakeLists
+    2. Clear CMakeLists
+    3. Build Project
+    4. Exit"
+    '''
+    ))
 
 
 def _get_parser() -> ap:
     parser = ap(description="Arguments to generate and/or build a C/C++ project with CMake.",
                 conflict_handler="resolve"
                 )
-
-    parser.add_argument("--action",
-                        type=str,
-                        required=True,
-                        choices=['g', 'b', 'c'],
-                        help="Specify the action: 'g' to generate, 'b' to build, 'c' to clear."
-                        )
 
     parser.add_argument("--project",
                         type=str,
@@ -23,12 +32,35 @@ def _get_parser() -> ap:
     parser.add_argument("--json",
                         type=str,
                         required=True,
-                        help="Path to the JSON file"
+                        help="Path to the setup JSON file"
                         )
 
     return parser
 
 
+def main(options):
+
+    # TODO
+    options.project
+    options.json
+
+    while True:
+        _print_menu()
+
+        choice = input("Enter your choice (1-4): ")
+
+        if choice == "1":
+            print("Generating CMakeLists...")
+        elif choice == "2":
+            print("Clearing CMakeLists...")
+        elif choice == "3":
+            print("Building Project...")
+        elif choice == "4":
+            print("Exiting... Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please enter a number between 1 and 4.")
+
+
 if __name__ == "__main__":
-    options = _get_parser().parse_args()
-    localimpl.main(options.action, options.project, options.json)
+    main(_get_parser().parse_args())
