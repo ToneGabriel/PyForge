fd2d0c147276754f56a8cdcdf5d9c3b35cd1ac66
ToneGabriel <81569579+ToneGabriel@users.noreply.github.com>
2025-07-03 21:51:28 +0300

    Update

--- DEV_LINUX_VENV.sh
+++ DEV_LINUX_VENV.sh
@@ -14,15 +14,15 @@ if [ -d "$VENV_DIR" ]; then
 else
     echo
     echo "[INFO] Creating virtual environment..."
-    py -m venv "$VENV_DIR"
+    python3 -m venv "$VENV_DIR"
 
     echo
     echo "[INFO] Activating and configuring environment..."
     source "$VENV_DIR/bin/activate"
 
     echo "[INFO] Upgrading pip and installing requirements..."
-    py -m pip install --upgrade pip
-    py -m pip install -r requirements.txt
+    python3 -m pip install --upgrade pip
+    python3 -m pip install -r requirements.txt
 
     deactivate
 fi
